cbaeb1409ae18aa2cc10f138e328a56ed147ee7b
Debra Kuang <146883196+dkuang4@users.noreply.github.com>
2023-10-03 13:08:06 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,10 +1,13 @@
 #This program prints the message "Hello world!"
 
+#This function prints "Hello, World!"
 def PrintHelloWorld():
 	print ("Hello, World!")
 
+#This defines the main function of the program
 def main():
 	PrintHelloWorld()
 
+#This runs the main function
 if __name__ == '__main__':
 	main()
