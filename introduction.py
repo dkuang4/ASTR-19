fce866c45e4bcc083b0a925104c878f0e601b090
Debra Kuang <146883196+dkuang4@users.noreply.github.com>
2023-10-03 13:50:35 -0700

    Create introduction.py

---
+++ introduction.py
@@ -0,0 +1,26 @@
+#This function prints my name
+def PrintName():
+  print("My name is Debra Kuang.")
+
+#This function prints my pronouns
+def PrintPronouns():
+  print('My pronouns are they/them.')
+
+#This function prints two sentences about my favorite movie
+def PrintMovie():
+  print('My favorite movie is Disney\'s Mulan (1998). I think the ending was well executed and it tells a good story, even if it is stylized.')
+
+#This function prints my favorite food
+def PrintFood():
+  print('My favorite food is fried tofu.')
+
+#This defines the main() function for the program
+def main():
+  PrintName()
+  PrintPronouns()
+  PrintMovie()
+  PrintFood()
+
+#This executes first when the program is run
+if __name__ == "__main__":
+  main()
