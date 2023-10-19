4795ae68673fd2a8bd3f2536f963f034b5fe0910
Debra Kuang <146883196+dkuang4@users.noreply.github.com>
2023-10-18 23:58:46 -0700

    Add files via upload
    This is the entry for Code Journal #1, prompt 2

---
+++ mathtest.py
@@ -0,0 +1,28 @@
+#this program is for Code Journal 1, prompt 2
+
+#define 2 floats
+x = 1.0
+y = 2.0
+
+#assign x the value of x + y
+z = x + y
+#print result + data type
+print(f'1.0 + 2.0 = {z}')
+print(type(z))
+
+#define 2 int
+x = 2
+y = 1
+
+#assign x the value of x - y
+z = x - y
+#print result + data type
+print(f'2 - 1 = {z}')
+print(type(z))
+
+#define a float, keep y as an int
+x = 1.0
+
+#print result + data type of x*y
+print(f'x * y = {x*y}')
+print(type(x))
